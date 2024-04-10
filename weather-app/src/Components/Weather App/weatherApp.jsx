8e570161006b1fe66db1de6273754b9ef5ad6633
Habi12345 <131537314+Habi12345@users.noreply.github.com>
2024-04-10 11:28:15 +0100

    Day 1: Getting some error message

---
+++ weather-app/src/Components/Weather App/weatherApp.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import './weatherApp.css'
+const weatherApp = () => {
+  return (
+    <div clssName='container'>
+        <div className='content-wraapper'>
+          <div className='card-wrapper'>
+            <div className='card'>
+
+            </div>
+          </div>
+        </div>
+    </div>
+      )
+}
+
+      export default weatherApp
