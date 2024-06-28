f90f7f99d2d980c19d81cc958b3f7ecf2a2af447
Habib Abdulkareem <131537314+Habi12345@users.noreply.github.com>
2024-06-28 08:42:03 +0100

    ..

--- real-time-chat-app/src/components/navbar.jsx
+++ real-time-chat-app/src/components/navbar.jsx
@@ -20,8 +20,8 @@ const Navbar = () => {
         <button onClick={handleLogout}>Logout</button>
       ) : (
         <div className='btn-wrapper'>
-          <Link to="/"><button data-aos="fade-right"  className='btn'>Login</button></Link>
-          <Link to="/register"><button data-aos="fade-left"  className='btn'>Register</button></Link>
+          <Link to="/"><button data-aos="fade-right" data-aos-once="true"  className='btn'>Login</button></Link>
+          <Link to="/register"><button data-aos="fade-left" data-aos-once="true"  className='btn'>Register</button></Link>
         </div>
       )}
     </nav>
