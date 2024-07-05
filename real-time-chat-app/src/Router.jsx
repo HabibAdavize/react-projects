39ea4e478eac41c42292b4d445a9e832f28e87cb
Habib Abdulkareem <131537314+Habi12345@users.noreply.github.com>
2024-07-05 09:02:43 +0100

    ..

--- real-time-chat-app/src/Router.jsx
+++ real-time-chat-app/src/Router.jsx
@@ -1,15 +1,19 @@
 // src/App.js
-import React, {useEffect} from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import AOS from 'aos';
-import 'aos/dist/aos.css';
-import { AuthProvider } from './contexts/AuthContext';
-import ChatPage from './pages/ChatPage';
-import LoginPage from './pages/LoginPage';
-import RegisterPage from './pages/RegisterPage';
+import React, { useEffect, useContext } from "react";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from "react-router-dom";
+import AOS from "aos";
+import "aos/dist/aos.css";
+import { AuthProvider } from "./contexts/AuthContext";
+import ChatPage from "./pages/ChatPage";
+import LoginPage from "./pages/LoginPage";
+import RegisterPage from "./pages/RegisterPage";
 
 const AppRouter = () => {
-
   useEffect(() => {
     AOS.init({ duration: 700 });
   }, []);
