4a01c83ea7e073b49f0c212831ba6c58fa9ca31a
HabibAdavize <131537314+HabibAdavize@users.noreply.github.com>
2024-08-13 18:26:40 +0100

    Did some Mobile and Desktop View

--- real-time-chat-app/src/components/UserList.jsx
+++ real-time-chat-app/src/components/UserList.jsx
@@ -12,13 +12,14 @@ import {
 import { db } from "../firebase";
 import { useAuth } from "../hooks/useAuth";
 import { useNavigate } from "react-router-dom";
-import Lottie from 'lottie-react';
+import Lottie from "lottie-react";
 import Chat from "../assets/animations/chat.json";
 
 const UserList = () => {
   const [users, setUsers] = useState([]);
   const [messages, setMessages] = useState({});
   const [searchTerm, setSearchTerm] = useState("");
+  const [isUserListVisible, setIsUserListVisible] = useState(false);
   const { currentUser } = useAuth();
   const navigate = useNavigate();
 
@@ -91,12 +92,18 @@ const UserList = () => {
         chatId = chatRef.id;
       }
 
-      navigate(`/chat/${chatId}`); // Navigate to ChatPage with the chatId
+      navigate(`/chat/${chatId}`);
+      setIsUserListVisible(false);
     } catch (error) {
       console.error("Error starting private chat:", error);
       // Display an error message to the user or log the error in a more robust way
     }
   };
+
+  const toggleUserList = () => {
+    setIsUserListVisible(!isUserListVisible);
+  };
+  
 
   const filteredUsers = users.filter(
     (user) =>
@@ -105,35 +112,49 @@ const UserList = () => {
   );
 
   return (
-    <div className="user-list">
-      <h2>Users</h2>
-      <input
-        type="text"
-        placeholder="Search users"
-        value={searchTerm}
-        onChange={(e) => setSearchTerm(e.target.value)}
-      />
-      <ul>
-        {filteredUsers.map((user) => (
-          <li key={user.id} onClick={() => startPrivateChat(currentUser, user)}>
-            <img src={user.profilePicture} alt={`${user.userName}'s profile`} />
-            <span>{user.userName}</span>
-            <ul>
-              {messages[user.id] &&
-                messages[user.id].map((message, index) => (
-                  <li key={index}>{message.content}</li>
-                ))}
-            </ul>
-            <Lottie
-                  animationData={Chat}
-                  loop={true}
-                  autoplay={true} // Adjust animation based on showPassword state
-                  style={{ width: 24, height: 24 }}
-                />
-          </li>
-        ))}
-      </ul>
+    <>
+
+    <div className="user-list-toggle">
+      <button onClick={toggleUserList}>
+        ===
+      </button>
     </div>
+      <div className={`user-list ${isUserListVisible ? "visible" : "hidden"}`}>
+        <h2>Users</h2>
+        <input
+          type="text"
+          placeholder="Search users"
+          value={searchTerm}
+          onChange={(e) => setSearchTerm(e.target.value)}
+        />
+        <ul>
+          {filteredUsers.map((user) => (
+            <li
+              key={user.id}
+              onClick={() => startPrivateChat(currentUser, user)}
+            >
+              <img
+                src={user.profilePicture}
+                alt={`${user.userName}'s profile`}
+              />
+              <span>{user.userName}</span>
+              <ul>
+                {messages[user.id] &&
+                  messages[user.id].map((message, index) => (
+                    <li key={index}>{message.content}</li>
+                  ))}
+              </ul>
+              <Lottie
+                animationData={Chat}
+                loop={true}
+                autoplay={true} // Adjust animation based on showPassword state
+                style={{ width: 24, height: 24 }}
+              />
+            </li>
+          ))}
+        </ul>
+      </div>
+    </>
   );
 };
 
